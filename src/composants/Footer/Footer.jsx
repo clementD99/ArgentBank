ee10b2682fd63e0106ddcb2433f372faa45d0e44
clementD99 <clement.desmur@gmail.com>
2024-12-19 14:20:02 +0100

    Ajout du footer

---
+++ src/composants/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import '../Footer/footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-text">Copyright 2024 Argent Bank</p>
+    </div>
+  );
+}
+
+export default Footer;
